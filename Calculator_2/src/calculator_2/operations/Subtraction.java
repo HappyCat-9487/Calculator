f16e85f5c106e9d78189fb9f0574b10b91886567
HappyCat-9487 <72371206+HappyCat-9487@users.noreply.github.com>
2022-03-16 04:01:41 +0800

    Added subtraction

---
+++ Calculator_2/src/calculator_2/operations/Subtraction.java
@@ -0,0 +1,19 @@
+package calculator_2.operations;
+
+public class Subtraction extends Operation {
+	
+	@Override
+	public String getName() {
+		return "subtraction";
+	}
+	
+	@Override
+	public String getOperator() {
+		return "-";
+	}
+	
+	@Override
+	public double calc() {
+		return a - b;
+	}
+}
